2d4bba5455eadec4eaacf7bf990ef040ded7d7a7
Daniel Rivera <dr734659@gmail.com>
2022-08-21 12:51:50 -0500

    [FIX] port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 
 const http = require("http").Server(app);
 const cors = require("cors");
